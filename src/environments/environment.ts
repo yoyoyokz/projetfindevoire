fa2dea3ae1bd9bf345607f541043a96cce1bd8c1
yoyoyokz <54137562+yoyoyokz@users.noreply.github.com>
2019-12-29 19:17:47 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCRojqxH7BMt6jZmBAVZH__2CtZWuQhWFg",
-    authDomain: "projetestaka.firebaseapp.com",
-    databaseURL: "https://projetestaka.firebaseio.com",
-    projectId: "projetestaka",
-    storageBucket: "projetestaka.appspot.com",
-    messagingSenderId: "2789271388",
-    appId: "1:2789271388:web:1a1d2407903ad2c778c399",
-    measurementId: "G-7J2J9JJ53K"
+    
   }
 };
